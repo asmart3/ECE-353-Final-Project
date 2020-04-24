d6a5f56c90a9404ef17bdaab152c0dc0733e4ef2
Matthew Beyer <mmbeyer3@wisc.edu>
2020-04-24 17:44:38 -0500

    Barriers now block movement

--- ECE-353-Final-Project/main.c
+++ ECE-353-Final-Project/main.c
@@ -4,8 +4,9 @@
 //*****************************************************************************
 // Global Vars
 //*****************************************************************************
-barrier barriers[1];
-
+barrier barriers[5];
+int numBarriers = 5;
+int SPEED = 4; //lower number is faster
 
 //*****************************************************************************
 //*****************************************************************************
@@ -25,36 +26,70 @@ void EnableInterrupts(void)
   }
 }
 
-
+//Check Tank Collision with Barrier
+bool checkCollision(int xPos, int yPos){
+	int i;
+	int rightSide,bottomSide;
+	
+	xPos /=SPEED;
+	yPos /=SPEED;
+	xPos -=16;
+	yPos -=16;
+	rightSide = xPos + upTankWidth;//+16;
+	bottomSide = yPos + upTankHeight;//+16;
+	
+	for(i = 0; i<5;i++){
+		if(!barriers[i].broken){
+			//check x position
+			if(((rightSide>=barriers[i].xPos)&&(rightSide<=(barriers[i].xPos+barriers[i].width))) || ((xPos <= (barriers[i].xPos + barriers[i].width)) && (xPos>=barriers[i].xPos))){
+				//check y position
+				if(((yPos>=barriers[i].yPos)&&(yPos<=(barriers[i].yPos+barriers[i].height))) || ((bottomSide <= (barriers[i].yPos + barriers[i].height)) && (bottomSide>=barriers[i].yPos))){
+					return true;
+				}
+			}
+		}
+	}
+	
+	return false;
+}
 
 
 // PlayerMovement
 void playerMove(tank *player,bool *alert_move){
-	if (ps2_get_x() > 0x7FF){
-			if((player->xPos/8 - (upTankWidth/2))>0){
-				player->direction = left;
-				player->xPos -= 1;
-				*alert_move = true;
+	if (ps2_get_x() > 0x8A5){
+			if((player->xPos/SPEED - (upTankWidth/2))>0){
+				if(!checkCollision(player->xPos -1,player->yPos)){
+					player->direction = left;
+					player->xPos -= 1;
+					*alert_move = true;
+				}
+				
 			}
 		}else if(ps2_get_x() < 0x700){
-			if((player->xPos/8 + (upTankWidth/2))<COLS){
-				player->direction = right;
-				player->xPos += 1;
-				*alert_move = true;
+			if((player->xPos/SPEED + (upTankWidth/2))<COLS){
+				if(!checkCollision(player->xPos+1,player->yPos)){
+					player->direction = right;
+					player->xPos += 1;
+					*alert_move = true;
+				}
 			}
 		}
 		
-		if (ps2_get_y() > 0x7FF){
-			if((player->yPos/8 - (upTankWidth/2))>0){
-				player->direction = up;
-				player->yPos -= 1;
-				*alert_move = true;
+		if (ps2_get_y() > 0x8A5){
+			if((player->yPos/SPEED - (upTankWidth/2))>0){
+				if(!checkCollision(player->xPos,player->yPos-1)){
+					player->direction = up;
+					player->yPos -= 1;
+					*alert_move = true;
+				}
 			}
 		}else if(ps2_get_y() < 0x700){
-			if((player->yPos/8 + (upTankWidth/2))<ROWS){
-				player->direction = down;
-				player->yPos += 1;
-				*alert_move = true;
+			if((player->yPos/SPEED + (upTankWidth/2))<ROWS){
+				if(!checkCollision(player->xPos,player->yPos+1)){
+					player->direction = down;
+					player->yPos += 1;
+					*alert_move = true;
+				}
 			}
 		}
 	
@@ -62,13 +97,26 @@ void playerMove(tank *player,bool *alert_move){
 
 
 void initBarriers(){
-	barrier block ={20,20,20,20,false};
-	barriers[0] = block;
+	int i;
+	barrier block ={40,40,40,40,false};
+	barrier block2 ={20,20,20,20,true};
+	barrier block3 ={180,220,35,35,false};
+	
+	barriers[0] = block; 
+	barriers[1] = block3;
+	for(i = 2;i<numBarriers;i++){
+		barriers[i] = block2;
+	}
+	
 	
 }
 
 void drawBarriers(){
-	lcd_draw_box(barriers[0].xPos,barriers[0].width,barriers[0].yPos,barriers[0].height,LCD_COLOR_RED,LCD_COLOR_BLACK,2);
+	int i;
+	for(i=0;i<numBarriers;i++){
+		if(!barriers[i].broken)
+			lcd_draw_box(barriers[i].xPos,barriers[i].width,barriers[i].yPos,barriers[i].height,LCD_COLOR_RED,LCD_COLOR_BLACK,2);
+	}
 	
 		
 }
@@ -78,8 +126,8 @@ int main(void)
 	bool alert_move = false;
 	bool game_over = false;
 	tank player;
-	player.xPos = 860;
-	player.yPos = 860;
+	player.xPos = 100 * SPEED;
+	player.yPos = 100 * SPEED;
 	player.direction = up;
 	
 		//initialize_serial_debug();
@@ -91,7 +139,7 @@ int main(void)
 	//	put_string("******************************\n\r");  
 
 		init_hardware();
-		lcd_draw_image(player.xPos/8,upTankWidth,player.yPos/8,upTankHeight,upTank,LCD_COLOR_GREEN,LCD_COLOR_BLACK);
+		lcd_draw_image(player.xPos/SPEED,upTankWidth,player.yPos/SPEED,upTankHeight,upTank,LCD_COLOR_GREEN,LCD_COLOR_BLACK);
 		initBarriers();
 	
 	while(!game_over){
@@ -101,16 +149,16 @@ int main(void)
 		if(alert_move){
 			switch(player.direction){
 				case up:
-					lcd_draw_image(player.xPos/8,upTankWidth,player.yPos/8,upTankHeight,upTank,LCD_COLOR_GREEN,LCD_COLOR_BLACK);
+					lcd_draw_image(player.xPos/SPEED,upTankWidth,player.yPos/SPEED,upTankHeight,upTank,LCD_COLOR_GREEN,LCD_COLOR_BLACK);
 					break;
 				case down:
-					lcd_draw_image(player.xPos/8,downTankWidth,player.yPos/8,downTankHeight,downTank,LCD_COLOR_GREEN,LCD_COLOR_BLACK);
+					lcd_draw_image(player.xPos/SPEED,downTankWidth,player.yPos/SPEED,downTankHeight,downTank,LCD_COLOR_GREEN,LCD_COLOR_BLACK);
 					break;
 				case left:
-					lcd_draw_image(player.xPos/8,leftTankWidth,player.yPos/8,leftTankHeight,leftTank,LCD_COLOR_GREEN,LCD_COLOR_BLACK);
+					lcd_draw_image(player.xPos/SPEED,leftTankWidth,player.yPos/SPEED,leftTankHeight,leftTank,LCD_COLOR_GREEN,LCD_COLOR_BLACK);
 					break;
 				case right:
-					lcd_draw_image(player.xPos/8,rightTankWidth,player.yPos/8,rightTankHeight,rightTank,LCD_COLOR_GREEN,LCD_COLOR_BLACK);
+					lcd_draw_image(player.xPos/SPEED,rightTankWidth,player.yPos/SPEED,rightTankHeight,rightTank,LCD_COLOR_GREEN,LCD_COLOR_BLACK);
 					break;
 			}
 				
