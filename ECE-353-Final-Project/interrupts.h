335caa5145b7ad6a41f55dc4486bf10277632aba
jenikaiser <63822176+jenikaiser@users.noreply.github.com>
2020-04-30 00:23:40 -0500

    Interrupt files for tank movement

---
+++ ECE-353-Final-Project/interrupts.h
@@ -0,0 +1,19 @@
+#ifndef __INTERRUPTS_H__
+#define __INTERRUPTS_H__
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "TM4C123.h"
+#include "gpio_port.h"
+#include "ps2.h"
+#include "lcd.h"
+#include "timers.h"
+#include "main.h"
+#include "images.h"
+#include "driver_defines.h"
+
+extern bool trigger_timer1;
+#endif
